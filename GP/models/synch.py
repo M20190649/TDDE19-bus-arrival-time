fc302a79c6b2e3c25c49ceaa8188b622dffcaec7
Sebastian Callh <sebastian.callh@gmail.com>
2018-11-12 13:57:01 +0100

    Extracted model build from training, improved plotting of posterior

--- GP/models/synch.py
+++ GP/models/synch.py
@@ -12,81 +12,43 @@ import seaborn as sns
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import mean_absolute_error
 
-def train(train_x, train_y, valid_x, valid_y, n_runs, draw_l, draw_sigmaf, draw_sigma):
+def build(X, Y, l_prior, sigmaf_prior, sigma_prior):
+    X_norm = __normalise(X)
+    Y_norm = __normalise(Y)
+    d = X.shape[1]
+
+    with gpflow.defer_build():
+        rbf = gpflow.kernels.RBF(d, ARD=True)
+        model = gpflow.models.GPR(X_norm, Y_norm, rbf)
+        model.kern.lengthscales.prior = l_prior
+        model.kern.variance.prior = sigmaf_prior
+        model.likelihood.variance.prior = sigma_prior
+
+    return model
+
+def train(model, n_runs, draw_l, draw_sigmaf, draw_sigma):
     """
     Trains n_runs times with random restarts from prior distributions
-    and returns a model with the parametrisation that performs the best 
-    on the validation data. The draw_* arguments are functions that draw from your chosen prior.
-    """
-    
-    # Scale data to zero mean and unit variance
-    train_x_norm = __normalise(train_x)
-    valid_x_norm = __normalise(valid_x)
-    d = train_x.shape[1]
-    
-    model = __build(train_x_norm, train_y)
-
-    def run(old_best, n):
-        l, sigmaf, sigma = draw_l(), draw_sigmaf(), draw_sigma() #ls[n, :], sigmafs[n], sigmas[n]
-        new_model = __set_params(model, l, sigmaf, sigma)
-        mae = __eval(new_model, valid_x_norm, valid_y)
-        return (mae, l, sigmaf, sigma) if  mae > old_best[0] else old_best
-
-    mae_max, l_max, sigmaf_max, sigma_max = reduce(run, range(n_runs), (-math.inf,))
-    return __set_params(model, l_max, sigmaf_max, sigma_max), mae_max
-
-def plot_predictions(model, data_x, data_y):
-    """
-    Plots the predictions of the models posterior mean function
-    against the true values of provided data.
-    """
-    mean, _var = model.predict_y(__normalise(data_x))
-    df = pd.DataFrame(np.hstack([data_x, data_y, mean]), columns=list('xypm'))
-    ax = sns.lineplot(x='p', y='m', data=df)
-    ax.set(xlabel='Ground truth', ylabel='Prediction')
-    return ax
-
-def plot_posterior_mean(model, data):
-    """
-    Plots the posterior mean of the model as a heatmap
-    in the region of the provided data, with the data overlayed
-    as a scatterplot. Assumes the data to be an n by 2 matrix of coordinates.
+    and returns a model with the parametrisation that maximises the log likelihod.
+    The draw_* arguments are functions that draw from your chosen prior.
     """
 
-    data_norm = __normalise(data)
-    
-    padding = 0 # Padding > 0 does not work when overlaying the data in scatterplot
-    norm_latmax = data_norm[:, 0].max() + padding
-    norm_latmin = data_norm[:, 0].min() - padding
-    norm_longmax = data_norm[:, 1].min() - padding
-    norm_longmin = data_norm[:, 1].max() + padding
+    def run(old_best, _n):
+        l, sigmaf, sigma = draw_l(), draw_sigmaf(), draw_sigma()
+        set_params(model, l, sigmaf, sigma)
+        gpflow.train.ScipyOptimizer().minimize(model)
+        loglik = model.compute_log_likelihood()
+        return (loglik, l, sigmaf, sigma) if  loglik > old_best[0] else old_best
 
-    d = 200 # resolution of the heatmap
-    xlist = np.linspace(norm_latmin, norm_latmax, d)
-    ylist = np.linspace(norm_longmin, norm_longmax, d)
-    xx, yy = np.meshgrid(xlist, ylist)
-    grid = np.array([np.reshape(xx,(-1,)).T, np.reshape(yy,(-1,))]).T
-    mean, _var = model.predict_y(grid)
-    
-    lat_unnorm = data['lat']
-    lon_unnorm = data['lon']
-    lat_grid = ((lat_unnorm-lat_unnorm.min())/(lat_unnorm.max() - lat_unnorm.min()))*d
-    lon_grid = ((lon_unnorm-lon_unnorm.min())/(lon_unnorm.max() - lon_unnorm.min()))*d
-    df_grid = pd.DataFrame({'lat':lat_grid, 'lon': lon_grid})
+    _loglik_max, l_max, sigmaf_max, sigma_max = reduce(run, range(n_runs), (-math.inf,))
+    set_params(model, l_max, sigmaf_max, sigma_max)
 
-    hm_grid = mean.reshape(d,d)[::-1] # Flip in y direction
-    ax = sns.heatmap(hm_grid, vmin=0, vmax=1, cmap="YlGnBu")
-    ax.invert_yaxis()
-    sns.scatterplot(x='lat', y='lon', data=df_grid, color=".2", axes=ax)
-    return ax
-    
 def load(X, Y, path):
     """
     Creates a new model and sets its parameters to the ones loaded from provided path.
     """
-    model = __build(X, Y)
+    model = build(X, Y, None, None, None)
     with open(path, 'rb') as handle:
         params = pickle.load(handle)
         model.assign(params)
@@ -99,19 +61,57 @@ def save(path, model):
     with open(path, 'wb') as handle:
         pickle.dump(model.read_trainables(), handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-def __max_under(xs, f):
+def plot_predictions(model, data_x, data_y):
     """
-    Returns the element that maximises the provided function.
+    Plots the predictions of the models posterior mean function
+    against the true values of provided data.
     """
-    max_under_f = lambda x, y: x if f(x) > f(y) else y
-    return reduce(max_under_f, xs, (-math.inf,))
+    mean, _var = model.predict_y(__normalise(data_x))
+    df = pd.DataFrame(np.hstack([data_x, data_y, mean]), columns=list('xypm'))
+    ax = sns.lineplot(x='p', y='m', data=df)
+    ax.set(xlabel='Ground truth', ylabel='Prediction')
+    return ax
 
-def __build(X, Y):
-    with gpflow.defer_build():
-        rbf = gpflow.kernels.RBF(X.shape[1], ARD=True)
-        model = gpflow.models.GPR(X, Y, rbf)
-    return model
+def plot_posterior_mean(model, X, Y):
+    """
+    Plots the posterior mean of the model as a heatmap
+    in the region of the provided data, with the data overlayed
+    as a scatterplot. Assumes the data to be an n by 2 matrix of coordinates.
+    """
 
+    X_norm = __normalise(X)
+    
+    padding = 0 # Padding > 0 does not work when overlaying the data in scatterplot
+    norm_latmax = X_norm[:, 0].max() + padding
+    norm_latmin = X_norm[:, 0].min() - padding
+    norm_longmax = X_norm[:, 1].min() - padding
+    norm_longmin = X_norm[:, 1].max() + padding
+
+    d = 200 # resolution of the heatmap
+    xlist = np.linspace(norm_latmin, norm_latmax, d)
+    ylist = np.linspace(norm_longmin, norm_longmax, d)
+    xx, yy = np.meshgrid(xlist, ylist)
+    grid = np.array([np.reshape(xx,(-1,)).T, np.reshape(yy,(-1,))]).T
+    mean, _var = model.predict_y(grid)
+    
+    lat_unnorm = X[:, 0]
+    lon_unnorm = X[:, 1]
+    lat_grid = ((lat_unnorm-lat_unnorm.min())/(lat_unnorm.max() - lat_unnorm.min()))*d
+    lon_grid = ((lon_unnorm-lon_unnorm.min())/(lon_unnorm.max() - lon_unnorm.min()))*d
+    progress = Y[:, 0]
+    df_grid = pd.DataFrame({'lat':lat_grid, 'lon': lon_grid, 'progress': progress})
+
+    hm_grid = mean.reshape(d,d)[::-1] # Flip in y direction
+    cmap = "YlGnBu"
+    ax = sns.heatmap(hm_grid, vmin=0, vmax=1, cmap=cmap)
+    ax.invert_yaxis()
+    sns.scatterplot(x='lat', y='lon', hue='progress', palette=cmap, data=df_grid, axes=ax)
+    return ax
+
+
+def gamma_prior(alpha, theta):
+    return gpflow.priors.Gamma(alpha, theta)
+         
 def __mape(pred_y, true_y):
     """
     MAPE will break when true data has 0 in it, which ours does so it is not used.
@@ -122,18 +122,17 @@ def __normalise(data):
     scaler = StandardScaler().fit(data)
     return scaler.transform(data)
 
-def __eval(model, valid_x, valid_y):
+def eval(model, metric, valid_x, valid_y):
     """
-    Evaluates the model using MAE.
+    Evaluates the model using provided metric.
     """
     gpflow.train.ScipyOptimizer().minimize(model)
     pred_y, _var = model.predict_y(valid_x)
-    return mean_absolute_error(valid_y, pred_y)
+    return metric(valid_y, pred_y)
 
-def __set_params(model, l, sigmaf, sigma):
+def set_params(model, l, sigmaf, sigma):
     model.clear()
     model.kern.lengthscales = l
     model.kern.variance = sigmaf
     model.likelihood.variance = sigma
     model.compile()
-    return model
