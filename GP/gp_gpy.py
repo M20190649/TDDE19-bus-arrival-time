a6f4073a6b32eadf82abbeacd8219f6747cd6599
Sebastian Callh <sebastian.callh@gmail.com>
2018-11-22 17:53:55 +0100

    Finish functionality to make weighted predictions
    Test data is not properly generated yet

--- GP/gp_gpy.py
+++ GP/gp_gpy.py
@@ -52,10 +52,10 @@ def __make_model(X: np.ndarray,
     writing to file.
     """
     model = GPy.models.GPRegression(X, Y,
-        GPy.kern.RBF(input_dim=X.shape[1],
-                     variance=1.,
-                     lengthscale=1.))
-    return GP(model, X, Y, name, route_n, traj_n, seg_n)
+                                    GPy.kern.RBF(input_dim=X.shape[1],
+                                                 variance=1.,
+                                                 lengthscale=1.))
+    return GP(model, X, Y, name, route_n, traj_n, seg_n) 
 
 def build_synch(data: pd.DataFrame,
                 X: List[str],
@@ -72,18 +72,17 @@ def build(data: pd.DataFrame,
           traj_n: int,
           seg_n: int) -> GP:
     """Wraps the model creation with a nices interface against pandas tables."""
-    #x_vals = scale(data[X].values)
-    #y_vals = data[Y].values
-    [x_vals, y_vals] = __normalize(data[X].values, data[Y].values)
+    x_vals = scale(data[X].values)
+    y_vals = data[Y].values
+    #[x_vals, y_vals] = __normalize(data[X].values, data[Y].values)
     return __make_model(x_vals, y_vals, name, route_n, traj_n, seg_n)
 
-def __normalize(X: np.ndarray, Y: np.ndarray):
-    x_max = np.amax(X)
-    y_max = np.amax(Y)
 
-    glob_max = max([x_max, y_max])
-
-    return [X/glob_max, Y/glob_max]
+def __normalize(X: np.ndarray):
+    lat_max = np.amax(X[:,0])
+    lon_max = np.amax(X[:,1])
+    glob_max = max([lat_max, lon_max])
+    return X/glob_max #, Y/glob_max]
 
 ## SAVE AND LOAD STUFF ##
 
@@ -103,7 +102,7 @@ def __gp_data_file(route_n: int, traj_n: int, seg_n: int) -> str:
     return __gp_file_name(route_n, traj_n, seg_n) + '.pkl'
 
 def __gp_file_info(file_path: str) -> (int, int, int):
-    m = re.match(r'./gps/pred-v1\\(\d+).(\d+).(\d+).*', file_path)
+    m = re.match(r'./gps/*/(\d+).(\d+).(\d+).*', file_path)
     return m.group(1), m.group(2), m.group(3)
 
 def save(gp: GP) -> None:
@@ -162,26 +161,24 @@ def load(name: str,
 
 def load_trajs(name: str, route_n: int, seg_n: int) -> List[GP]:
     """
-    Loads all GPs trained for provided trajectory.
+    Loads all GPs with the given name, trained on trajectories for provided route and segment.
     """
     file_reg = __gp_path(name) + str(route_n) + '.*.' + str(seg_n)
     data_reg = file_reg  + '.pkl'
     param_reg = file_reg + '.npy'
     datas = [load_data(p) for p in glob.glob(data_reg)]
     params = [load_params(p) for p in glob.glob(param_reg)]
-    def from_params(data, params):
+    traj_ns = re.findall(r'\d.(\d).\d.pkl', ''.join(glob.glob(data_reg)))
+    def from_params(data, params, traj_n):
         X = data[0]
         Y = data[1]
-        model = __make_model(X, Y, name, route_n, 0, seg_n)
+        model = __make_model(X, Y, name, route_n, traj_n, seg_n)
         return set_params(model, params)
 
-    return [from_params(d, p) for d, p in zip(datas, params)]
-
-    
+    return [from_params(d, p, t) for d, p, t in zip(datas, params, traj_ns)]
 
 #    spara arrival time och modellen bara
     # Find all .npy files in name save dir
-    
 #    params = [load_gp(path) for path in file_paths]
  #   return {r: {t: {s: p for _, _, s, p in params}
   #              for _, t, _, _ in params}
